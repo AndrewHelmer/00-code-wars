8158616e61a89b6772ad691e8e788e0685a33ea1
Andrew Helmer <ahelmer7@live.com>
2018-07-25 13:21:05 -0700

    done with kata

---
+++ highest-scoring-word/solution.js
@@ -0,0 +1,17 @@
+high = (x) => {
+  const words = x.split(' ');
+  let highIndex = 0;
+  let highValue = 0;
+  let currentValue = 0;
+  words.forEach((aWord, index) => {
+    console.log(aWord);
+    currentValue =
+      aWord.split('').reduce((accumulator, currentValue) => accumulator + currentValue.charCodeAt(0)%96,0);
+    console.log(currentValue);
+    if(currentValue > highValue) {
+      highIndex = index;
+      highValue = currentValue;
+    }
+  });
+  return words[highIndex];
+};
